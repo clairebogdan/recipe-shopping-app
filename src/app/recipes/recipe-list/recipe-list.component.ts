52264da21e27448d875da78869a1b4e11df87597
clairebogdan <bogdanclaire@gmail.com>
2020-11-30 08:08:35 -0500

    monday work start

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Cookies', 'chocolate chip', 'https://c.pxhere.com/photos/b1/b3/CC0_Photos_cf_choc_chip_Chocolate_chocolate_chip_Cookies_Free_Images_Free_Photos-1616507.jpg!d')
+  ];
   constructor() { }
 
   ngOnInit(): void {
